d1950d9a30d730c16ffe6a754abddd7a3296e281
helloangely <aphilip@yipitdata.com>
2019-12-11 15:44:36 -0500

    setup tests

--- tests/unit/test_limits.py
+++ tests/unit/test_limits.py
@@ -1,30 +1,25 @@
-from __future__ import absolute_import
-
-
 import moto
 from unittest import TestCase
+from mock import patch
 import os
 
+mock_env = {
+    'ROLE_ARN': 'arn:aws:iam::212311232123:role/test',
+    'SENDGRID_API_KEY': '1231231231231231231',
+}
+with patch.dict(os.environ, mock_env):
+    from awslimits.support import create_or_get_table
 
-class DynamoHelpersTestCase(TestCase):
-    def setUp(self):
-        os.environ['ROLE_ARN'] = 'arn:aws:iam::212311232123:role/test'
-        os.environ['SENDGRID_API_KEY'] = '1231231231231231231'
-        from awslimits.support import create_or_get_table
 
-    # unset these env vars
-
+class TestDynamo(TestCase):
     def test_create_or_get_existing_table(self):
-        pass
-
+        create_or_get_table()
 
     def test_create_or_get_new_table(self):
         pass
 
-
     def test_create_or_get_table_exception(self):
         pass
-
 
     def test_create_or_get_table_failed(self):
         pass
