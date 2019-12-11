86be4968e4a6335cd122d5c2537d992750637a73
helloangely <aphilip@yipitdata.com>
2019-12-11 16:08:20 -0500

    added intial test for creating new table

--- tests/unit/test_limits.py
+++ tests/unit/test_limits.py
@@ -1,4 +1,5 @@
 import moto
+import boto3
 from unittest import TestCase
 from mock import patch
 import os
@@ -6,16 +7,42 @@ import os
 mock_env = {
     'ROLE_ARN': 'arn:aws:iam::212311232123:role/test',
     'SENDGRID_API_KEY': '1231231231231231231',
+    'AWS_ACCESS_KEY_ID': 'awskey',
+    'AWS_SECRET_ACCESS_KEY': 'awssecret',
 }
 with patch.dict(os.environ, mock_env):
     from awslimits.support import create_or_get_table
 
 
+@moto.mock_dynamodb2
+@moto.mock_sts
 class TestDynamo(TestCase):
-    def test_create_or_get_existing_table(self):
-        create_or_get_table()
-
     def test_create_or_get_new_table(self):
+        dynamodb = boto3.client('dynamodb', region_name="us-east-1")
+        table_name = 'awslimits_limits'
+        initial_table_list = dynamodb.list_tables()
+        assert len(initial_table_list["TableNames"]) == 0
+        table = create_or_get_table(
+            table_name='awslimits_limits',
+            attribute_definitions=[
+                {
+                    'AttributeName': 'limit_name',
+                    'AttributeType': 'S',
+                },
+            ],
+            key_schema=[
+                {
+                    'AttributeName': 'limit_name',
+                    'KeyType': 'HASH'
+                },
+            ],
+        )
+        assert table.name == 'awslimits_limits'
+        final_table_list = dynamodb.list_tables()
+        assert len(final_table_list["TableNames"]) == 1
+
+
+    def test_create_or_get_exiting_table(self):
         pass
 
     def test_create_or_get_table_exception(self):
