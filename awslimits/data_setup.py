20f67ed02cd31d88a897b6e7ac3c93c482ea1684
Jim Shields <jim.j.shields@gmail.com>
2020-03-27 16:55:02 -0400

    Fix print statements for Python 3

--- awslimits/data_setup.py
+++ awslimits/data_setup.py
@@ -2,12 +2,12 @@ from .support import load_tickets, load_default_limits
 import settings
 
 def update_data():
-    print 'loading default limits...'
+    print('loading default limits...')
     load_default_limits()
     if settings.PREMIUM_ACCOUNT:
-        print 'loading tickets...'
+        print('loading tickets...')
         load_tickets()
-    print 'done'
+    print('done')
 
 if __name__ == "__main__":
     update_data()
